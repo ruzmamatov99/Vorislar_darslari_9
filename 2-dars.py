17d2ecd0f231f60c0e01327be239df398b62d0ac
ruzmamatov99 <kruzmamatov2010@gmail.com>
2025-10-09 16:28:27 +0300

    2-dars

---
+++ 2-dars.py
@@ -0,0 +1,27 @@
+# Bugundan Python bilan ishlaymiz
+# Print() consolga malumot chiqarish buyryg'i
+
+print('Hello, World!')
+print("Hello, World!")
+
+# Arifmetik amallar
+# Qo'shish
+a = 12
+b = 34
+c = a + b
+print('c =', c)
+
+# Ayirish
+v = a - b
+print('v =', v)
+ 
+# Ko'paytirish
+m = a * b    # Ko'paytirish
+print('m =', m)   # darajaga ko'tarish
+
+# Bo'lish
+g = 5
+p1 = g / 2
+p2 = g // 2
+print('p1 =', p1)
+print('p2 =', p2)
