a78a81a516465576d1901d4b1b2ec08be906720a
ruzmamatov99 <kruzmamatov2010@gmail.com>
2025-10-16 15:14:41 +0300

    masalalar

---
+++ 3-dars.py
@@ -0,0 +1,31 @@
+import math
+
+#ism = input("Ismingiz nima? :")
+#print("Assalomu Alaykum hurmatli, kelajaki buyuk", ism)
+
+#son = int(input("Ixtiyoriy son kiriting :"))
+#print("Kritilgan sonning kvadrati:", son*son)
+
+#son = int(input("Kvadratning tomonini yozing :"))
+#print("Kvadratning perimetri :", son*4)
+
+#son = int(input("Kvadratning tomonini yozing :"))
+#print("Kvadratning yuzi :", son*son)
+
+# boyi = int(input("To'g'ri to'rtburchakningning bo'yini yozing :"))
+# eni = int(input("To'g'ri to'rtburchakningning enini yozing :"))
+# print("To'g'ri to'rtburchakning yuzi:", eni*boyi)
+# print("To'g'ri to'rtburchakning perimetri", (eni+boyi)*2)
+
+# 14.10.2025
+# Doiraning radiusi R berilgan. Uzunligi L va yuzasi S aniqlansin. L=2*п*R  S=п*(R*R)
+# radius = int(input("Doiraning radiusini yozing : R = "))
+# print("Doiraning uzunligi (L = 2 * п * R), L=", 2*3.14*radius)
+# print("Doiraning yuzasi(S = п * (R*R)) S=", 3.14*(radius*radius))
+
+# Sonlar o'qida ikkita nuqta orasidagi masofa aniqlansin. |x2-x1|
+# x1 = int(input("Sonlar o'qida x1 ni yozing ="))
+# x2 = int(input("Sonlar o'qida x2 ni yozing ="))
+# print("X2 va x1 orasidagi masofani moduldan chiqarish (|x2-x1|) =", abs(x2-x1))
+
+# x ning qiymati berilganda y=3x**6-6x**2-7 funksiyaning qiymati aniqlansin.
