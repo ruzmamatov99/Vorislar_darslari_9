fcb28eb8edf1f03cd9d749deb5a775201f973200
ruzmamatov99 <kruzmamatov2010@gmail.com>
2025-10-25 16:41:06 +0300

    funksiyalar

---
+++ 8-dars(Ro'yxatlar_methods_and_function).py
@@ -0,0 +1,44 @@
+"""
+25.10.2025
+Ro'yxatlar bilan ishlash. Methodlar va function bilan ishlash
+methods(.sort())
+function(sum(), min(), max(), sorted(), list())
+Ro'yxatlardan nusxa olish
+tuple() o'zgarmas ro'yxat
+"""
+
+# davlatlar = ["Uzbekistan", "Brazilya", "Ispaniya", "Vatikan", "Qazag'istan", "Turkiya"]
+# mashinalar = list(("BMW", "audi", "tico", "damas", "depal s07", "depal s103", "onix"))
+# mashinalar.sort()
+# sonlar = list((12, 5, 7, 8, -12))
+# sonlar.sort(reverse=True)
+# print(sonlar)
+# print(davlatlar)
+# print(mashinalar)
+
+# sonlar1 = list(range(0,21))
+# print(sonlar1)
+
+# sonlar2 = list(range(-8,30,3))
+# print(len(sonlar2))
+
+# sonlar = [89,-213,45,-12,-64,234,568,-8]
+# print(max(sonlar))
+# print(min(sonlar))
+# print(sum(sonlar))
+
+# davlatlar = ["Uzbekistan", "Brazilya", "Ispaniya", "Vatikan", "Qazag'istan", "Turkiya"]
+# country = davlatlar
+# print("davlatlar nomli ro'yxat:", davlatlar)
+# print("country nomli ro'yxat:", country)
+
+# country.insert(0,"singapur")
+# print(country)
+
+# print(davlatlar)
+# print(country)
+
+toqson = list(range(2,21,2))
+print("Asl ko'rinishi :", toqson)
+print("1 dan 7 gacha:", toqson[0:4])
+print(toqson[:])
