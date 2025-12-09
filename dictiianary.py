bb1f25288d7b925bc349042462f67e0d71bf3432
ruzmamatov99 <kruzmamatov2010@gmail.com>
2025-12-09 16:35:20 +0300

    k

---
+++ dictiianary.py
@@ -0,0 +1,22 @@
+# ismlar = ['Ilyosbek', 'mardonboy', 'murodbek', 'karimboy']
+# thisdict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year" : 1964
+# }
+# print(thisdict)
+
+# kamolbek = {
+#     'ism' : 'kamolbek',
+#     'yosh' : 15,
+#     'familya': 'rozmamatov',
+#     'manzil': 'qoraqosh',
+#     'maktab': '1-sonli maktab',
+#     'sinf': 9,
+#     'qiziqish': 'futbol',
+#     'transport': 'Gelik',
+#     'oqish': 'oliy'
+# }
+
+# print(f"{kamolbek['ism'].title()} {kamolbek['familya'].title()} {kamolbek['yosh']} yoshda. U {kamolbek['manzil'].title()} qishlog'ida yashaydi. Kamolbek {kamolbek['maktab']}ning {kamolbek['sinf']} sinfida o'qiydi. U {kamolbek['qiziqish']}ga qiziqadi. Kamolbek {kamolbek['transport']} mashinasida yuradi. Uning o'qishi {kamolbek['oqish']} darajada")
+
