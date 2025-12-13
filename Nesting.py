eecd33a35f31c5998168f2e09ffba82bb57759c1
ruzmamatov99 <kruzmamatov2010@gmail.com>
2025-12-13 16:02:59 +0300

    nesting

---
+++ Nesting.py
@@ -0,0 +1,113 @@
+talaba1 = {
+    'ism': 'kamolbek',
+    'familya' : 'rozmamatov',
+    'maktab': '1-maktab',
+    'qiziqish': 'futbol'
+}
+
+talaba2 = {
+    'ism': 'boboxon',
+    'familya' : 'jumaboyev',
+    'maktab': '12-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+talaba3 = {
+    'ism': 'ixtiyor',
+    'familya' : 'baxtiyorov',
+    'maktab': '26-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+talaba4 = {
+    'ism': 'elbek',
+    'familya' : 'jaborrov',
+    'maktab': '46-maktab',
+    'qiziqish': 'ingliz tili'
+}
+
+talaba5 = {
+    'ism': 'xolbek',
+    'familya' : 'qurbonboyev',
+    'maktab': '1-maktab',
+    'qiziqish': 'futbol'
+}
+
+talaba6 = {
+    'ism': 'ilyosbek',
+    'familya' : 'qochqorboyev',
+    'maktab': '1-maktab',
+    'qiziqish': 'kitob oqish'
+}
+
+talaba7 = {
+    'ism': 'umidbek',
+    'familya' : 'matyoqubov',
+    'maktab': '1-maktab',
+    'qiziqish': 'kibersport'
+}
+
+talaba8 = {
+    'ism': 'zuhriddin',
+    'familya' : 'qaadamboyev',
+    'maktab': '26-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+talaba9 = {
+    'ism': 'muhammadali',
+    'familya' : 'ibragimov',
+    'maktab': '1-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+talaba10 = {
+    'ism': 'baxtiyor',
+    'familya' : 'ozodov',
+    'maktab': '1-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+talaba11 = {
+    'ism': 'sharofat',
+    'familya' : 'jumamurodova',
+    'maktab': '1-maktab',
+    'qiziqish': 'koreys darslari'
+}
+
+talaba12 = {
+    'ism': 'matluba',
+    'familya' : 'olloyorova',
+    'maktab': '1-maktab',
+    'qiziqish': 'kitob oqish'
+}
+
+talaba13 = {
+    'ism': 'maftuna',
+    'familya' : 'madaminova',
+    'maktab': '1-maktab',
+    'qiziqish': 'fizika'
+}
+
+talaba14 = {
+    'ism': 'mashhura',
+    'familya' : 'shomurotova',
+    'maktab': '1-maktab',
+    'qiziqish': 'IT darslari'
+}
+
+malumot_tiplari = {
+    'str': 'string',
+    'int': 2025 ,
+    'float': 2025.3,
+    'bool': True,
+    'list': ['malibu', 'cobalt', 'matiz'],
+    'tuple': ('pi', 'g', 'k', 'R'),
+    'dictinary':{
+        'kitoblar':{
+            'badiy': 'sariq devni minib',
+            'hajviy': 'ikki karra ikki besh'
+        },
+        'badiy_kitob':'ikki eshik orasi'
+    }
+    }
